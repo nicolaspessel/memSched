46fca6fc88920a7243f1a03405bd0f97c25a3587
nicolaspessel <nicolaspessel@gmail.com>
2026-06-12 10:57:35 -0300

    feat: implement process life cycle with PCB and burst time, still developing

--- src/Kernel.java
+++ src/Kernel.java
@@ -1,9 +1,38 @@
+import java.util.LinkedList;
 import java.util.Queue;
 
-public class Kernel implements SchedulingStrategy{
+public class Kernel {
+    private final MemoryManager memoryManager;
+    private final SchedulingStrategy schedulingStrategy; // Composition
 
-    @Override
-    public Process selectNext(Queue<Process> readyQueue) {
-        return null;
+    private final Queue<ProcessControlBlock> readyQueue = new LinkedList<>(); // Better O(n) complexity
+    private final ProcessControlBlock currentlyRunning = null;
+    private int clock = 0;
+
+    // Constructor
+    public Kernel(MemoryManager memoryManager, SchedulingStrategy schedulingStrategy) {
+        this.memoryManager = memoryManager;
+        this.schedulingStrategy = schedulingStrategy;
+    }
+
+    // Inner class
+    private static class ProcessControlBlock {
+        private Process process;
+        private int remainingTicks;
+        private ProcessState currentState;
+
+        // Constructor
+        public ProcessControlBlock(Process process) {
+            this.process = process;
+            this.remainingTicks = process.burstTime();
+            this.currentState = ProcessState.READY;
+        }
+    }
+
+    // Methods
+    public void addNewProcess(Process process) {
+        memoryManager.addNewProcess(process);
+        ProcessControlBlock pcb = new ProcessControlBlock(process);
+        readyQueue.add(pcb);
     }
 }
