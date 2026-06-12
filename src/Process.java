b516ac05a03d20a6f150ac96b13e2e481624ccc6
nicolaspessel <nicolaspessel@gmail.com>
2026-06-12 09:57:07 -0300

    feat: create process life cycle based on time and state

--- src/Process.java
+++ src/Process.java
@@ -1,4 +1,4 @@
-public record Process(int pid, int requiredMemory, int priority) {
+public record Process(int pid, int requiredMemory, int priority, int burstTime) {
     public int calculateSwapScore(int memoryWeight, int priorityWeight) {
         return (memoryWeight * this.requiredMemory) - (priorityWeight * this.priority);
     }
---
+++ src/ProcessState.java
@@ -0,0 +1,5 @@
+public enum ProcessState {
+    READY, // In the queue, waiting for the CPU
+    RUNNING, // Currently running on the CPU
+    SWAPPED // Swapped to the disk
+}
