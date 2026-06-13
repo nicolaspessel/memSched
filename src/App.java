00f8a3003671773f57ff66bc57fd9f5687761626
nicolaspessel <nicolaspessel@gmail.com>
2026-06-13 16:00:53 -0300

    feat: add startSimulation method with loop for tests and results view

--- src/App.java
+++ src/App.java
@@ -18,5 +18,15 @@ public class App {
         Scheduler scheduler = new Scheduler();
 
         Kernel kernel = new Kernel(memoryManager, scheduler);
+
+        System.out.println("[BOOT] Loading initializing processes...");
+
+        Process p1 = new Process(1, "Terminal", 1000000, 3, 5);
+        Process p2 = new Process(2, "EditorTexto", 500000, 2, 3);
+
+        kernel.addNewProcess(p1);
+        kernel.addNewProcess(p2);
+
+        kernel.startSimulation();
     }
 }
